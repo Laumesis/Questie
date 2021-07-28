a33f0f701f70272b1429b6a6e419f93f626e12ac
Laumesis <75958501+Laumesis@users.noreply.github.com>
2021-07-28 16:02:45 +0300

    fix scopes in _QuestieTracker:ConvertSetPointCords -variables xOff, yOff global -> local -Initializing those is unnecessary, but I did it for future bug safety. -I left xOff = xOn and yOff = xOff assigns into  if branches for code readibility. #3078

--- Modules/Tracker/QuestieTrackerPrivates.lua
+++ Modules/Tracker/QuestieTrackerPrivates.lua
@@ -290,8 +290,9 @@ function _QuestieTracker:ConvertSetPointCords(frame, setpoint)
         local setPoint = setpoint
         local xOn = ({frame:GetPoint()})[4]
         local yOn = ({frame:GetPoint()})[5]
-        if not (height or width or preSetPoint or xOn or yOn or setPoint or preSetPoint)then return end
+        if not (height or width or preSetPoint or xOn or yOn or setPoint or preSetPoint) then return end
         if (setPoint ~= preSetPoint) then
+            local xOff, yOff = xOn, yOn
 
             -- To "TOPLEFT" from...
             if setPoint == "TOPLEFT" then
